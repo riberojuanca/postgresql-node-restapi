d3481c6ad4ea4b02337dd0ba4e2b380f2ac3fadf
Juanca Ribero <riberojuanca@gmail.com>
2024-08-10 17:46:29 -0300

    Test completed

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,60 @@
+import { pool } from "../db.js";
+
+export const getUsers = async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM users");
+  res.json(rows);
+};
+
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await pool.query("SELECT * FROM users WHERE id= $1", [id]);
+  if (rows.length === 0) {
+    return res.status(404).json({ message: `User ${id} not found.` });
+  }
+  res.json(rows[0]);
+};
+
+export const createUser = async (req, res) => {
+  try {
+    const data = req.body;
+    const { rows } = await pool.query(
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+      [data.name, data.email]
+    );
+    return res.json(rows[0]);
+  } catch (error) {
+    if (error?.code === "23505") {
+      console.log(`${error.detail}`);
+      return res.status(409).json({ message: "Email already exists" });
+    }
+
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  const { rows, rowsCount } = await pool.query(
+    "DELETE FROM users WHERE id = $1 RETURNING *",
+    [id]
+  );
+
+  if (rowsCount === 0) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+  return res.sendStatus(204);
+};
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  const { rows } = await pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+    [data.name, data.email, id]
+  );
+
+  return res.json(rows[0]);
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,28 +1,19 @@
 import { Router } from "express";
 import { pool } from "../db.js";
+import {
+  getUsers,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.controllers.js";
 
 const router = Router();
 
-router.get("/users", (req, res) => {
-  res.send("Obteniendo usuarios");
-});
-
-router.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  res.send("Obteniendo usuario " + id);
-});
-
-router.post("/users", (req, res) => {
-  res.send("Creando usuario");
-});
-
-router.delete("/users/:id", (req, res) => {
-  res.send("Eliminando usuario");
-});
-
-router.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  res.send("Actualizando usuario " + id);
-});
+router.get("/users", getUsers);
+router.get("/users/:id", getUser);
+router.post("/users", createUser);
+router.delete("/users/:id", deleteUser);
+router.put("/users/:id", updateUser);
 
 export default router;
